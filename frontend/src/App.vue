28605db5f68471b45f91f082cf2b3d2d67b063bf
sebas <sebastianwaltilla@hotmail.com>
2020-05-06 12:10:22 +0200

    added structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,64 @@
 <template>
   <div id="app">
+
+    <h1> Nu börjar det på riktigt </h1>
+    <!-- Document Header Starts
+    // HEAD (menu)
+  -------------------------------------------------
+    APPEN allt innehåll
+
+    (if true)
+    // Start sida       komponent
+
+    (if true)
+    // log in       komponent
+
+    (if true)
+    // single page        komponent
+
+    (if true)
+    // barn       komponent
+
+    (if true)
+    // lärare       komponent
+
+    (if true)
+    // game!           komponent
+
+    -----------------------------------------------
+    // footer komponent
+    -->
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <h1> HEJ FRÅN GRUPPEN! </h1>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      head,
+      startPage,
+      studentPage,
+      teacherPage,
+      footer,
+      HelloWorld
+
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
