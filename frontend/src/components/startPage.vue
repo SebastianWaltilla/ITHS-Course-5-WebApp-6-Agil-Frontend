46711aab670a345c7d5e760a647c201d2ad7e629
sebas <sebastianwaltilla@hotmail.com>
2020-05-06 14:54:31 +0200

    adding start page component

---
+++ frontend/src/components/startPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class = "frame">
+
+
+        <p> info how to play </p>
+
+
+        <ul>
+            <li>        <button> Single play   </button>   </li>
+            <li>        <button>  Enter room code  </button>   </li>
+            <li>        <button>   Login  </button>   </li>
+        </ul>
+
+
+
+
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "startPage"
+    }
+</script>
+
+<style scoped>
+
+         ul li {
+             display: inline;
+             padding: 20px;
+         }
+</style>
